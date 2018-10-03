4ac0589bd8a0f0a5e5d47af40a0885e97c360330
Archanciel <jp.schnyder@gmail.com>
2018-10-03 20:33:44 +0200

    posted version

--- draw_grid/draw_grid_using_rect_or_line.py
+++ draw_grid/draw_grid_using_rect_or_line.py
@@ -3,149 +3,106 @@
 # Video link: https://www.youtube.com/watch?v=uWvb3QzA48c
 # Project setup
 
-import pygame as pg
+import pygame
 
-from draw_grid.player import Player
-from draw_grid.settings import *
 import os
 import time
 
+TITLE = "Draw grid"
+WINDOWS_LOCATION = '100,100'
+WIDTH = 710
+HEIGHT = 710
+FPS = 1
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+CELL_SIZE = 15
+GRID_COORD_MARGIN_SIZE = 20
+
 class Grid():
-    def __init__(self, surface, cellSize, initCellValue):
+    def __init__(self, surface, cellSize, axisLabels):
         self.surface = surface
         self.colNb = surface.get_width() // cellSize
         self.lineNb = surface.get_height() // cellSize
         self.cellSize = cellSize
-        self.initCellValue = initCellValue
-#        self.grid = [[initCellValue] * self.columns for i in range(self.lines)]
-        self.grid = [[initCellValue for i in range(self.colNb)] for j in range(self.lineNb)]
-        self.font = pg.font.SysFont('arial', 12, False)
+        self.axisLabels = axisLabels
+        self.grid = [[0 for i in range(self.colNb)] for j in range(self.lineNb)]
+        self.font = pygame.font.SysFont('arial', 12, False)
 
     def drawUseRect(self):
         for li in range(self.lineNb):
             liCoord = GRID_COORD_MARGIN_SIZE + li * CELL_SIZE
-            if li < 10:
-                ident = '   '
-            else:
-                ident = '  '
-            text = self.font.render(ident + str(li), 1, (0, 0, 0))
-            self.surface.blit(text, (0, liCoord))
+            if self.axisLabels:
+                if li < 10:
+                    ident = '   '
+                else:
+                    ident = '  '
+                text = self.font.render(ident + str(li), 1, (0, 0, 0))
+                self.surface.blit(text, (0, liCoord))
             for co in range(self.colNb):
                 colCoord = GRID_COORD_MARGIN_SIZE + co * CELL_SIZE
+                if self.axisLabels:
+                    if co < 10:
+                        ident = '  '
+                    else:
+                        ident = ' '
+                    text = self.font.render(ident + str(co), 1, (0, 0, 0))
+                    self.surface.blit(text, (colCoord, 1))
+                pygame.draw.rect(self.surface, BLACK, pygame.Rect(liCoord, colCoord, CELL_SIZE, CELL_SIZE), 1)
+
+    def drawUseLine(self):
+        for li in range(self.lineNb):
+            liCoord = GRID_COORD_MARGIN_SIZE + li * CELL_SIZE
+            if self.axisLabels:
+                if li < 10:
+                    ident = '   '
+                else:
+                    ident = '  '
+                text = self.font.render(ident + str(li), 1, (0, 0, 0))
+                self.surface.blit(text, (0, liCoord))
+            pygame.draw.line(self.surface, BLACK, (GRID_COORD_MARGIN_SIZE, liCoord), (self.surface.get_width(), liCoord))
+        for co in range(self.colNb):
+            colCoord = GRID_COORD_MARGIN_SIZE + co * CELL_SIZE
+            if self.axisLabels:
                 if co < 10:
                     ident = '  '
                 else:
                     ident = ' '
                 text = self.font.render(ident + str(co), 1, (0, 0, 0))
                 self.surface.blit(text, (colCoord, 1))
-                pg.draw.rect(self.surface, BLACK, pg.Rect(liCoord, colCoord, CELL_SIZE, CELL_SIZE), 1)
+            pygame.draw.line(self.surface, BLACK, (colCoord, GRID_COORD_MARGIN_SIZE), (colCoord,self.surface.get_height()))
 
-    def drawUseLine(self):
-        for li in range(self.lineNb):
-            liCoord = GRID_COORD_MARGIN_SIZE + li * CELL_SIZE
-            if li < 10:
-                ident = '   '
-            else:
-                ident = '  '
-            text = self.font.render(ident + str(li), 1, (0, 0, 0))
-            self.surface.blit(text, (0, liCoord))
-            pg.draw.line(self.surface, BLACK, (GRID_COORD_MARGIN_SIZE, liCoord), (self.surface.get_width(), liCoord))
-        for co in range(self.colNb):
-            colCoord = GRID_COORD_MARGIN_SIZE + co * CELL_SIZE
-            if co < 10:
-                ident = '  '
-            else:
-                ident = ' '
-            text = self.font.render(ident + str(co), 1, (0, 0, 0))
-            self.surface.blit(text, (colCoord, 1))
-            pg.draw.line(self.surface, BLACK, (colCoord, GRID_COORD_MARGIN_SIZE), (colCoord,self.surface.get_height()))
+# setting Pygame window position (ok on Windows ...)
+os.environ['SDL_VIDEO_WINDOW_POS'] = WINDOWS_LOCATION
 
-class Game:
-    def __init__(self):
-        '''
-        Initializes game window, etc.
-        '''
-        # setting Pygame window position
-        os.environ['SDL_VIDEO_WINDOW_POS'] = WINDOWS_LOCATION
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
+clock = pygame.time.Clock()
+running = True
+axisLabels = False
 
-        pg.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption(TITLE)
-        self.clock = pg.time.Clock()
-        self.running = True
+grid = Grid(surface=screen, cellSize=CELL_SIZE, axisLabels=axisLabels)
 
-        self.grid = Grid(self.screen, CELL_SIZE, 0)
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
 
-    def new(self):
-        '''
-        Starts a new game.
-        '''
-        self.all_sprites = pg.sprite.Group()
-#        self.player = Player()
-#        self.all_sprites.add(self.player)
+    screen.fill(WHITE)
 
-    def run(self):
-        '''
-        Is the game loop.
-        '''
-        self.playing = True
+    start = time.time()
+    grid.drawUseRect()
+    print("draw using rect: {}".format(time.time() - start))
 
-        while self.playing:
-            self.clock.tick(FPS)
-            self.handleEvents()
-            self.update()
-            self.draw()
+    start = time.time()
+    grid.drawUseLine()
+    print("draw using line: {}".format(time.time() - start))
 
-    def handleEvents(self):
-        '''
-        Acquires and handles events.
-        '''
-        for event in pg.event.get():
-            # check for closing window
-            if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
 
-    def update(self):
-        '''
-        Updates all game objects.
-        '''
-        self.all_sprites.update()
-
-    def draw(self):
-        '''
-        Redraws all game objects.
-        '''
-        self.screen.fill(WHITE)
-        self.all_sprites.draw(self.screen)
-        start = time.time()
-        self.grid.drawUseRect()
-        print("draw using rect: {}".format(time.time() - start))
-        start = time.time()
-        self.grid.drawUseLine()
-        print("draw using line: {}".format(time.time() - start))
-        # *after* drawing everything, flip the display
-        pg.display.flip()
-
-    def show_start_screen(self):
-        '''
-        Shows game splash/start screen.
-        '''
-        pass
-
-    def show_go_screen(self):
-        '''
-        Shows game over screen.
-        '''
-        pass
-
-g = Game()
-g.show_start_screen()
-
-while g.running:
-    g.new()
-    g.run()
-    g.show_go_screen()
-
-pg.quit()
+pygame.quit()
