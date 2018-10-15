af1af3ed44514fef03d7fabf999b387a6aaa1bf0
Archanciel <jp.schnyder@gmail.com>
2018-10-15 17:52:50 +0200

    solved zoom in out line drawing and active cell drawing

--- draw_grid/grid.py
+++ draw_grid/grid.py
@@ -10,12 +10,14 @@ class Grid():
         self.initCellValue = initCellValue
         self.setGridDimension()
 
-        '''
-        Dimensioning the internal value grid to the max displayable cell number.
-        Since one cell can occupy a minuimum of 1 px and the grid line width
-        is 1 px, 2 cells will require
-        '''
-        self.cellValueGrid = [[initCellValue for i in range(surface.get_width() // 2)] for j in range(surface.get_height() // 2)]
+
+        #Dimensioning the internal value grid to the max displayable cell number.
+        #Since one cell can occupy a minuimum of 1 px and the grid line width
+        #is 1 px, 2 cells will require 1 + 1 + 1 + 1 + 1 = 5 px.
+        #3 cells require 1 + 1 + 1 + 1 + 1 + 1 + 1 = 7 px.
+        #n cells require (n * 2) + 1 px
+        self.cellValueGrid = [[initCellValue for i in range((surface.get_width() - 1) // 2)] for j in range((surface.get_height() - 1) // 2)]
+
         self.setStartPattern()
         self.font = pg.font.SysFont('arial', int(GRID_COORD_MARGIN_SIZE / 20 * 12), False)
         self.drawAxisLabel = True
@@ -43,8 +45,8 @@ class Grid():
             self.cellValueGrid[i][30] = True
 
     def setGridDimension(self):
-        self.colNb = self.surface.get_width() // self.cellSize
-        self.lineNb = self.surface.get_height() // self.cellSize
+        self.colNb = (self.surface.get_width() - 1) // (self.cellSize + 1)
+        self.lineNb = (self.surface.get_height() - 1) // (self.cellSize + 1)
 
     def draw(self):
         if self.drawAxisLabel:
@@ -52,8 +54,8 @@ class Grid():
         else:
             gridCoordMargin = 0
 
-        for li in range(self.lineNb + 1):
-            liCoord = gridCoordMargin + li * self.cellSize
+        for li in range(self.lineNb + 1): #+1 since the bottom most margin needs to be drawn !
+            liCoord = gridCoordMargin + li * (self.cellSize + 1)
 
             if self.drawAxisLabel:
                 if li < 10:
@@ -64,8 +66,8 @@ class Grid():
                 self.surface.blit(text, (0, liCoord))
 
             pg.draw.line(self.surface, BLACK, (gridCoordMargin, liCoord), (self.surface.get_width(), liCoord))
-        for co in range(self.colNb + 1):
-            colCoord = gridCoordMargin + co * self.cellSize
+        for co in range(self.colNb + 1): #+1 since the right most margin needs to be drawn !
+            colCoord = gridCoordMargin + co * (self.cellSize + 1)
 
             if self.drawAxisLabel:
                 if co < 10:
@@ -84,14 +86,13 @@ class Grid():
                 if self.cellValueGrid[row][col]:
                     pg.draw.rect(self.surface,
                                      GREEN,
-                                     [gridCoordMargin + ((1 + self.cellSize) * col) + 1,
-                                      gridCoordMargin + ((self.cellSize) * row) + 1,
-                                      self.cellSize - 1,
-                                      self.cellSize - 1])
+                                     [gridCoordMargin + 1 + ((1 + self.cellSize) * col),
+                                      gridCoordMargin + 1 + ((1 + self.cellSize) * row),
+                                      self.cellSize,
+                                      self.cellSize])
 
     def zoomIn(self):
         delta = self.cellSize // 10
-        print('zoom in: cellsize {}, delta {}'.format(self.cellSize, delta))
         if delta <= 0:
             delta = 1
 
@@ -104,10 +105,10 @@ class Grid():
             self.drawAxisLabel = True
 
         self.setGridDimension()
+        print('zoom in: cellsize {}, delta {}, col nb {}'.format(self.cellSize, delta, self.colNb))
 
     def zoomOut(self):
         delta = self.cellSize // 10
-        print('zoom out: cellsize {}, delta {}'.format(self.cellSize, delta))
 
         if delta <= 0:
             delta = 1
@@ -116,6 +117,6 @@ class Grid():
             self.cellSize -= delta
             if self.cellSize <= 11:
                 self.drawAxisLabel = False
-        print('end zoom out: cellsize {}, delta {}'.format(self.cellSize, delta))
+        print('zoom out: cellsize {}, delta {}, colnb {}'.format(self.cellSize, delta, self.colNb))
 
         self.setGridDimension()
