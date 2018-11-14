c350b9cde181a51affa433d469fc9f29e7e765ee
Archanciel <jp.schnyder@gmail.com>
2018-11-14 20:28:59 +0100

    improv comment

--- draw_grid/midcell.py
+++ draw_grid/midcell.py
@@ -3,31 +3,31 @@ from draw_grid.settings import *
 class MidCell():
     '''
     Thia class calculates the horizontal (x) and vertical (y) offset caused by a zoom in or out operation.
-    These offset will be used by the GridView class to reposition the grid view so that the zoomed zone
+    These offsets will be used by the GridView class to reposition the grid view so that the zoomed zone
     remains centered in the view.
     '''
     def __init__(self, gridView):
         '''
-        The MidCell instance is created before the zoom operation. It records the coordinates of the cell
-        positioned at the center of the grid view so they can be compared to the mid cell coordinates
-        after the zoom occured in  order to adjust the grid view positioning, if possible, so that the
-        mid cell remains centered.
+        The MidCell instance is created before the zoom operation takes place. It records the coordinates of
+        the cell positioned at the center of the grid view so that they can be compared to the mid cell
+        coordinates after the zoom occured in  order to adjust the grid view positioning, if possible, so that
+        the mid cell remains centered.
 
         :param gridView:
         '''
         self.gridView = gridView
 
         # computing the row/col index of the cell displaed in the middle of the grid view before the zoom occurs
-        self.rowBeforeZoom = gridView.startDrawRowIndex + (gridView.drawnedRowNb // 2) - 1 # minus 1 since 0 based !
-        self.colBeforeZoom = gridView.startDrawColIndex + (gridView.drawnedColNb // 2) - 1
+        self.midCellRowIndexBeforeZoom = gridView.startDrawRowIndex + (gridView.drawnedRowNb // 2) - 1 # minus 1 since 0 based !
+        self.midCellColIndexolBeforeZoom = gridView.startDrawColIndex + (gridView.drawnedColNb // 2) - 1
 
         # computing the top left x and y coordinates of the mid cell before the zoom operation
 
-        self.topLeftXCoordBeforeZoom = gridView.gridCoordMargin + GRID_LINE_WIDTH + gridView.gridOffsetXPx + (
-                (GRID_LINE_WIDTH + gridView.cellSize) * self.colBeforeZoom)
+        self.midCellTopLeftXCoordBeforeZoom = gridView.gridCoordMargin + GRID_LINE_WIDTH + gridView.gridOffsetXPx + (
+                (GRID_LINE_WIDTH + gridView.cellSize) * self.midCellColIndexolBeforeZoom)
 
-        self.topLeftYCoordBeforeZoom = gridView.gridCoordMargin + GRID_LINE_WIDTH + gridView.gridOffsetYPx + (
-                (GRID_LINE_WIDTH + gridView.cellSize) * self.rowBeforeZoom)
+        self.midCellTopLeftYCoordBeforeZoom = gridView.gridCoordMargin + GRID_LINE_WIDTH + gridView.gridOffsetYPx + (
+                (GRID_LINE_WIDTH + gridView.cellSize) * self.midCellRowIndexBeforeZoom)
 
     def computeXYOffsetAfterZoom(self):
         '''
@@ -39,13 +39,13 @@ class MidCell():
         # computing the top left x and y coordinates of the mid cell after the zoom operation
 
         topLeftXCoordPxAfterZoom = self.gridView.gridCoordMargin + GRID_LINE_WIDTH + self.gridView.gridOffsetXPx + (
-                (GRID_LINE_WIDTH + self.gridView.cellSize) * self.colBeforeZoom)
+                (GRID_LINE_WIDTH + self.gridView.cellSize) * self.midCellColIndexolBeforeZoom)
 
         topLeftYCoordPxAfterZoom = self.gridView.gridCoordMargin + GRID_LINE_WIDTH + self.gridView.gridOffsetYPx + (
-                (GRID_LINE_WIDTH + self.gridView.cellSize) * self.rowBeforeZoom)
+                (GRID_LINE_WIDTH + self.gridView.cellSize) * self.midCellRowIndexBeforeZoom)
 
         # calculating the x and y offset caused ba the zoom operation
-        zoomXOffsetPx = self.topLeftXCoordBeforeZoom - topLeftXCoordPxAfterZoom
-        zoomYOffsetPx = self.topLeftYCoordBeforeZoom - topLeftYCoordPxAfterZoom
+        zoomXOffsetPx = self.midCellTopLeftXCoordBeforeZoom - topLeftXCoordPxAfterZoom
+        zoomYOffsetPx = self.midCellTopLeftYCoordBeforeZoom - topLeftYCoordPxAfterZoom
 
         return zoomXOffsetPx, zoomYOffsetPx
