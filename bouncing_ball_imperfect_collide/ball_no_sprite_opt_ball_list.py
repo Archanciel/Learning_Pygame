d9a81c20ee5a42e43d4b0750010d01d1297cc864
Archanciel <jp.schnyder@gmail.com>
2020-04-19 16:50:19 +0200

    not working !

---
+++ bouncing_ball_imperfect_collide/ball_no_sprite_opt_ball_list.py
@@ -0,0 +1,63 @@
+import pygame as pg
+import math
+from settings import *
+
+class Ball(pg.sprite.Sprite):
+	def __init__(self, 
+	             screen, 
+	             allBalls, 
+	             index,
+				 ballNumber,
+	             color, 
+	             radius, 
+	             startX, 
+	             startY, 
+	             speed, 
+	             angle=-45):
+		super().__init__()
+		self.screen = screen
+		self.allBalls = allBalls
+		self.index = index
+		self.maxBallIndex = ballNumber - 1
+		self.color = color
+		rectSize = radius * 2
+		self.rect = pg.Rect(startX, startY, rectSize, rectSize)
+		self.speed = speed
+		self.angle = math.radians(-angle)
+		pg.draw.circle(self.screen, self.color, self.rect.center, int(self.rect.width / 2))
+
+	def update(self):
+		delta_x = self.speed * math.cos(self.angle)
+		delta_y = self.speed * math.sin(self.angle)
+		self.rect = self.rect.move(delta_x, delta_y)
+		hit_bounds = False
+		
+		if self.rect.right >= self.screen.get_width() or self.rect.left <= 0:
+			self.angle = math.pi - self.angle
+			hit_bounds = True
+			
+		if self.rect.top <= 0 or self.rect.bottom >= self.screen.get_height():
+			self.angle = -self.angle
+			hit_bounds = True
+
+		for i in range(self.index + 1, self.maxBallIndex):
+			ball = self.allBalls[i]
+			if not hit_bounds and self.collideBall(ball):
+				self.angle = self.angle - math.pi
+				break
+
+	def collideBall(self, ball):
+		'''
+		Returns True if self collides with ball.
+		'''
+		xDiff = (self.rect.centerx - ball.rect.centerx)
+		yDiff = (self.rect.centery - ball.rect.centery)
+		hypothenuseSquared = xDiff * xDiff + yDiff * yDiff
+		sumOfRadiuses = self.rect.width / 2 + ball.rect.width / 2
+		bang = hypothenuseSquared <= sumOfRadiuses * sumOfRadiuses
+
+		return bang
+
+	# Draw our ball to the screen
+	def draw(self):
+		pg.draw.circle(self.screen, self.color, self.rect.center, int(self.rect.width / 2))
