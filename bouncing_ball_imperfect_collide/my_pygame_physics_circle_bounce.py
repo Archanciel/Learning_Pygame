8d6aeca27e3a5ba9288979032ffc900d6087b9b4
Archanciel <jp.schnyder@gmail.com>
2020-05-03 17:12:04 +0200

    renaming + os specifics

--- bouncing_ball_imperfect_collide/my_pygame_physics_circle_bounce.py
+++ bouncing_ball_imperfect_collide/my_pygame_physics_circle_bounce.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+import os
 
 background_colour = (255,255,255)
 (width, height) = (0, 0)
@@ -19,16 +20,16 @@ def addVectors(angle1, length1, angle2, length2):
 
 def findParticle(particles, x, y):
     for p in particles:
-        if math.hypot(p.x-x, p.y-y) <= p.size:
+        if math.hypot(p.centerX - x, p.centerY - y) <= p.radius:
             return p
     return None
 
 def collide(p1, p2):
-    dx = p1.x - p2.x
-    dy = p1.y - p2.y
+    dx = p1.centerX - p2.centerX
+    dy = p1.centerY - p2.centerY
     
     dist = math.hypot(dx, dy)
-    if dist < p1.size + p2.size:
+    if dist < p1.radius + p2.radius:
         tangent = math.atan2(dy, dx)
         angle = 0.5 * math.pi + tangent
 
@@ -40,39 +41,39 @@ def collide(p1, p2):
         (p1.angle, p1.speed) = (angle1, speed1)
         (p2.angle, p2.speed) = (angle2, speed2)
 
-        p1.x += math.sin(angle)
-        p1.y -= math.cos(angle)
-        p2.x -= math.sin(angle)
-        p2.y += math.cos(angle)
+        p1.centerX += math.sin(angle)
+        p1.centerY -= math.cos(angle)
+        p2.centerX -= math.sin(angle)
+        p2.centerY += math.cos(angle)
 
 class Particle():
     
-    def __init__(self, x, y, size, boundary_circle_x, boundary_circle_y, boundary_circle_r):
-        self.boundary_circle_x = boundary_circle_x
-        self.boundary_circle_y = boundary_circle_y
-        self.boundary_circle_r = boundary_circle_r
+    def __init__(self, centerX, centerY, radius, boundaryCircleCenterX, boundaryCircleCenterY, boundaryCircleRadius):
+        self.boundaryCircleCenterX = boundaryCircleCenterX
+        self.boundaryCircleCenterY = boundaryCircleCenterY
+        self.boundaryCircleRadius = boundaryCircleRadius
 
-        self.x = x
-        self.y = y
-        self.size = size
+        self.centerX = centerX
+        self.centerY = centerY
+        self.radius = radius
         self.colour = (0, 0, 255)
         self.thickness = 0
         self.speed = 0
         self.angle = 0
 
     def display(self):
-        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
+        pygame.draw.circle(screen, self.colour, (int(self.centerX), int(self.centerY)), self.radius, self.thickness)
 
     def move(self):
         #(self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
-        self.x += math.sin(self.angle) * self.speed
-        self.y -= math.cos(self.angle) * self.speed
+        self.centerX += math.sin(self.angle) * self.speed
+        self.centerY -= math.cos(self.angle) * self.speed
         self.speed *= drag
 
     def bounce(self):
-        dx = self.boundary_circle_x - self.x
-        dy = self.boundary_circle_y - self.y
-        overlap = math.hypot(dx, dy) - (self.boundary_circle_r - self.size)
+        dx = self.boundaryCircleCenterX - self.centerX
+        dy = self.boundaryCircleCenterY - self.centerY
+        overlap = math.hypot(dx, dy) - (self.boundaryCircleRadius - self.radius)
 
         if overlap >= 0:
             tangent = math.atan2(dy, dx)
@@ -80,45 +81,50 @@ class Particle():
             self.speed *= elasticity
 
             angle = 0.5 * math.pi + tangent
-            self.x += math.sin(angle)*overlap
-            self.y -= math.cos(angle)*overlap
+            self.centerX += math.sin(angle) * overlap
+            self.centerY -= math.cos(angle) * overlap
             
-        if self.x > width - self.size:
-            self.x = 2*(width - self.size) - self.x
+        if self.centerX > width - self.radius:
+            self.centerX = 2 * (width - self.radius) - self.centerX
             self.angle = - self.angle
             self.speed *= elasticity
 
-        elif self.x < self.size:
-            self.x = 2*self.size - self.x
+        elif self.centerX < self.radius:
+            self.centerX = 2 * self.radius - self.centerX
             self.angle = - self.angle
             self.speed *= elasticity
 
-        if self.y > height - self.size:
-            self.y = 2*(height - self.size) - self.y
+        if self.centerY > height - self.radius:
+            self.centerY = 2 * (height - self.radius) - self.centerY
             self.angle = math.pi - self.angle
             self.speed *= elasticity
 
-        elif self.y < self.size:
-            self.y = 2*self.size - self.y
+        elif self.centerY < self.radius:
+            self.centerY = 2 * self.radius - self.centerY
             self.angle = math.pi - self.angle
             self.speed *= elasticity
 
+if os.name == 'posix':
+    screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+else:
+    WINDOWS_LOCATION = '100,25'
+    os.environ['SDL_VIDEO_WINDOW_POS'] = WINDOWS_LOCATION
+    screen = pygame.display.set_mode((800, 800))
 
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 width, height = screen.get_size()
 pygame.display.set_caption('Tutorial 8')
-boundary_circle_x = width / 2
-boundary_circle_y = height / 2
-boundary_circle_r = width / 2.1
-number_of_particles = 15
+boundaryCircleCenterX = width / 2
+boundaryCircleCenterY = height / 2
+boundaryCircleRadius = width / 2.1
+number_of_particles = 2
 my_particles = []
 
 for n in range(number_of_particles):
-    size = random.randint(50, 80)
-    x = random.randint(size, width-size)
-    y = random.randint(size, height-size)
+    radius = random.randint(50, 80)
+    centerX = random.randint(radius, width - radius)
+    centerY = random.randint(radius, height - radius)
 
-    particle = Particle(x, y, size, boundary_circle_x, boundary_circle_y, boundary_circle_r)
+    particle = Particle(centerX, centerY, radius, boundaryCircleCenterX, boundaryCircleCenterY, boundaryCircleRadius)
     particle.speed = random.random()
     particle.angle = random.uniform(0, math.pi*2)
 
@@ -139,13 +145,16 @@ while running:
 
     if selected_particle:
         (mouseX, mouseY) = pygame.mouse.get_pos()
-        dx = mouseX - selected_particle.x
-        dy = mouseY - selected_particle.y
+        dx = mouseX - selected_particle.centerX
+        dy = mouseY - selected_particle.centerY
         selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
         selected_particle.speed = math.hypot(dx, dy) * 0.1
 
     screen.fill(background_colour)
-    pygame.draw.circle(screen, (0,0,0), (boundary_circle_x, boundary_circle_y), boundary_circle_r, 1)
+    if os.name == 'posix':
+        pygame.draw.circle(screen, (0,0,0), (boundaryCircleCenterX, boundaryCircleCenterY), boundaryCircleRadius, 1)
+    else:
+        pygame.draw.circle(screen, (0, 0, 0), (int(round(boundaryCircleCenterX)), int(round(boundaryCircleCenterY))), int(round(boundaryCircleRadius)), 1)
 
     for i, particle in enumerate(my_particles):
         particle.move()
