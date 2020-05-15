50bd3c33571fd9a1e364d9758a5e9bb3c7dff67a
Archanciel <jp.schnyder@gmail.com>
2020-05-15 18:36:04 +0200

    added pos display

--- bouncing_ball_imperfect_collide/move_particle_basic.py
+++ bouncing_ball_imperfect_collide/move_particle_basic.py
@@ -2,7 +2,7 @@ import pygame
 import math
 import random
 import os
-from particle import Particle
+from particledisplaypos import ParticleDisplayPos
 
 PN = 500
 DIST_MIN = 1
@@ -17,6 +17,7 @@ else:
 	os.environ['SDL_VIDEO_WINDOW_POS'] = '100,15'
 	screen = pygame.display.set_mode((width, height))
 
+pygame.init()
 pygame.display.set_caption('Move particle basic')
 (w, h) = screen.get_size()
 
@@ -29,8 +30,8 @@ else:
 	circleY = w / 2
 	circleR = (h / 2) - 105
 
-my_particles = []
-my_particles2 = []
+#my_particles = []
+#my_particles2 = []
 my_particles3 = []
 
 angleTwelth = 360 / 12
@@ -38,15 +39,15 @@ angleTwelth = 360 / 12
 if os.name == 'posix':
 	for i in range(1, 13):
 		angleDeg = i * angleTwelth
-		my_particles.append(Particle(screen = screen, x=circleX, y=circleY, radius=25, thickness=3, angleDeg=angleDeg, speed=2))
-		my_particles2.append(Particle(screen = screen, x=circleX, y=circleY, radius=15, thickness=3, angleDeg=angleDeg, speed=1))
-		my_particles3.append(Particle(screen = screen, x=circleX, y=circleY, radius=10, thickness=3, angleDeg=angleDeg, speed=0.8))
+#		my_particles.append(ParticleDisplayPos(screen = screen, x=circleX, y=circleY, radius=25, thickness=3, angleDeg=angleDeg, speed=2))
+#		my_particles2.append(ParticleDisplayPos(screen = screen, x=circleX, y=circleY, radius=15, thickness=3, angleDeg=angleDeg, speed=1))
+		my_particles3.append(ParticleDisplayPos(screen = screen, x=circleX, y=circleY, radius=100, thickness=3, angleDeg=angleDeg, speed=0.8))
 else:
 	for i in range(1, 13):
 		angleDeg = i * angleTwelth
-		my_particles.append(Particle(screen = screen, x=circleX, y=circleY, radius=25, thickness=1, angleDeg=angleDeg, speed=0.2))
-		my_particles2.append(Particle(screen = screen, x=circleX, y=circleY, radius=15, thickness=1, angleDeg=angleDeg, speed=0.1))
-		my_particles3.append(Particle(screen = screen, x=circleX, y=circleY, radius=10, thickness=1, angleDeg=angleDeg, speed=0.08))
+#		my_particles.append(ParticleDisplayPos(screen = screen, x=circleX, y=circleY, radius=25, thickness=1, angleDeg=angleDeg, speed=0.2))
+#		my_particles2.append(ParticleDisplayPos(screen = screen, x=circleX, y=circleY, radius=15, thickness=1, angleDeg=angleDeg, speed=0.1))
+		my_particles3.append(ParticleDisplayPos(screen = screen, x=circleX, y=circleY, radius=100, thickness=1, angleDeg=angleDeg, speed=0.08))
 
 running = True
 
@@ -61,13 +62,13 @@ while running:
 	else:
 		pygame.draw.circle(screen, (0, 0, 255), (round(circleX), round(circleY)), round(circleR), 1)
 
-	for particle in my_particles:
-		particle.move()
-		particle.display()	
+#	for particle in my_particles:
+#		particle.move()
+#		particle.display()	
 
-	for particle in my_particles2:
-		particle.move_tuto()
-		particle.display()	
+#	for particle in my_particles2:
+#		particle.move_tuto()
+#		particle.display()	
 
 	for particle in my_particles3:
 		particle.my_move()
---
+++ bouncing_ball_imperfect_collide/particledisplaypos.py
@@ -0,0 +1,59 @@
+import pygame as pg
+import math
+import os
+
+from particle import Particle
+
+BLACK = (0, 0, 0)
+
+class ParticleDisplayPos(Particle):
+	def __init__(self, screen, x, y, radius, thickness, angleDeg, speed):
+		# angleDeg is the clockwise angle with 0 deg corresponding to 12 hour
+		super().__init__(screen, x, y, radius, thickness, angleDeg, speed)
+		
+		# Position related instance variables
+		self.font = pg.font.Font(None, 32)
+		self.font_height = self.font.get_linesize()
+
+		# Calculating the left margin based on longest possible text
+		rectSize = radius * 2
+		self.textHorizontalSize = self.font.size("angle: 360")[0]
+		self.textLeftMargin = (rectSize - self.textHorizontalSize) / 2
+
+		# Calculating the top margin based on the number of text lines
+		self.lineNumber = 3
+		self.textTopMargin = radius - (self.font_height * self.lineNumber / 2)
+		self.textColor = BLACK
+
+	def display(self):
+		super().display()
+
+		# Writing information on the ball surface if the ball size is large enough
+		if self.textHorizontalSize <= self.radius * 2:
+			self.blitTextOnBall(round(self.x, 2), round(self.y, 2))
+
+	def blitTextOnBall(self, xValue, yValue):
+		textLines = [None] * self.lineNumber
+		textLines[0] = 'x: ' + str(xValue)
+		textLines[1] = 'y: ' + str(yValue)
+
+		if (self.angleRad < 0):
+			# negative degrees are nonsensical !
+			angleDegree = round(math.degrees(2 * math.pi + self.angleRad))
+		else:
+			angleDegree = round(math.degrees(self.angleRad))
+
+		textLines[2] = 'angle: ' + str(angleDegree)
+
+		self.images = []  # The text surfaces.
+
+		for line in textLines:
+			textSurface = self.font.render(line, True, self.textColor)
+			self.images.append(textSurface)
+		for y, textSurface in enumerate(self.images):
+			if y * self.font_height + self.font_height > (2 * self.radius):
+				# Don't blit below the rect area.
+				break
+			textCoordinatesTuple = (self.x - self.radius + self.textLeftMargin,
+									self.y - self.radius + self.textTopMargin + y * self.font_height)
+			self.screen.blit(textSurface, textCoordinatesTuple)
